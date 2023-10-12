c0f9b3699544ff1f5eae4df6c3f7262056d23e46
Flo2410 <florian@hye.dev>
2023-10-12 14:17:42 +0000

    added lidar_test launch file

---
+++ src/mobi_rviz/launch/lidar_test.launch.py
@@ -0,0 +1,42 @@
+import launch
+from launch.substitutions import LaunchConfiguration
+import launch_ros
+import os
+
+
+def generate_launch_description():
+    pkg_share = launch_ros.substitutions.FindPackageShare(package="mobi_rviz").find("mobi_rviz")
+    default_rviz_config_path = os.path.join(pkg_share, "rviz/rplidar.rviz")
+
+    lidar_node = launch_ros.actions.Node(
+        name="rplidar_composition",
+        package="rplidar_ros",
+        executable="rplidar_composition",
+        output="screen",
+        parameters=[
+            {
+                "serial_port": "/dev/ttyUSB0",
+                "serial_baudrate": 115200,  # A1 / A2
+                # 'serial_baudrate': 256000, # A3
+                "frame_id": "laser",
+                "inverted": False,
+                "angle_compensate": True,
+            }
+        ],
+    )
+
+    rviz_node = launch_ros.actions.Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen",
+        arguments=["-d", LaunchConfiguration("rvizconfig")],
+    )
+
+    return launch.LaunchDescription(
+        [
+            launch.actions.DeclareLaunchArgument(name="rvizconfig", default_value=default_rviz_config_path, description="Absolute path to rviz config file"),
+            lidar_node,
+            rviz_node,
+        ]
+    )
