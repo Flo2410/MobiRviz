4d775d45afb8ca8e5fb934eb709c12093c7aa609
Flo2410 <florian@hye.dev>
2023-10-12 14:17:54 +0000

    added remote_controll launch file

---
+++ src/mobi_rviz/launch/remote_controll.launch.py
@@ -0,0 +1,28 @@
+import launch
+from launch.substitutions import LaunchConfiguration
+import launch_ros
+import os
+
+
+def generate_launch_description():
+    pkg_share = launch_ros.substitutions.FindPackageShare(package="mobi_rviz").find("mobi_rviz")
+    default_rviz_config_path = os.path.join(pkg_share, "rviz/rplidar.rviz")
+
+    rviz_node = launch_ros.actions.Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen",
+        arguments=["-d", LaunchConfiguration("rvizconfig")],
+    )
+
+    # rosrun teleop_twist_keyboard teleop_twist_keyboard.py
+    # teleop_key_node = launch_ros.actions.Node(package="teleop_twist_keyboard", executable="teleop_twist_keyboard", name="teleop_twist_keyboard")
+
+    return launch.LaunchDescription(
+        [
+            launch.actions.DeclareLaunchArgument(name="rvizconfig", default_value=default_rviz_config_path, description="Absolute path to rviz config file"),
+            rviz_node,
+            # teleop_key_node
+        ]
+    )
