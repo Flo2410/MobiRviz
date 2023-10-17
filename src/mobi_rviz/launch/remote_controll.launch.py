4ee8a1f234652121ee460878f880783715e092e5
Flo2410 <florian@hye.dev>
2023-10-17 17:22:48 +0200

    added robot launch file

--- src/mobi_rviz/launch/remote_controll.launch.py
+++ src/mobi_rviz/launch/remote_controll.launch.py
@@ -8,6 +8,7 @@ def generate_launch_description():
     pkg_share = launch_ros.substitutions.FindPackageShare(package="mobi_rviz").find("mobi_rviz")
     default_rviz_config_path = os.path.join(pkg_share, "rviz/rplidar.rviz")
 
+    # Run rviz2
     rviz_node = launch_ros.actions.Node(
         package="rviz2",
         executable="rviz2",
@@ -16,7 +17,7 @@ def generate_launch_description():
         arguments=["-d", LaunchConfiguration("rvizconfig")],
     )
 
-    # rosrun teleop_twist_keyboard teleop_twist_keyboard.py
+    # ros2 run teleop_twist_keyboard teleop_twist_keyboard
     # teleop_key_node = launch_ros.actions.Node(package="teleop_twist_keyboard", executable="teleop_twist_keyboard", name="teleop_twist_keyboard")
 
     return launch.LaunchDescription(
