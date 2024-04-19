671860aa0ad1768f1efcf419a5d7c556981c686f
Flo2410 <florian@hye.dev>
2024-04-19 11:25:14 +0200

    Added cam node

--- src/mobi_rviz/launch/robot.launch.py
+++ src/mobi_rviz/launch/robot.launch.py
@@ -8,6 +8,7 @@ def generate_launch_description():
     pkg_share = FindPackageShare("mobi_rviz")
     joy_params = PathJoinSubstitution([pkg_share, "config/joystick.yaml"])
     lidar_params = PathJoinSubstitution([pkg_share, "config/lidar.yaml"])
+    cam_params = PathJoinSubstitution([pkg_share, "config/cam.yaml"])
 
     # Lidar
     lidar_node = Node(
@@ -24,10 +25,14 @@ def generate_launch_description():
     # telepo (/joy -> /cmd_vel)
     teleop_node = Node(package="teleop_twist_joy", executable="teleop_node", name="teleop_node", parameters=[joy_params])
 
+    # cam_node = Node(package="mobipy", executable="cam", name="cam_publisher")
+    cam_node = Node(package="usb_cam", executable="usb_cam_node_exe", name="cam_node", parameters=[cam_params])
+
     return LaunchDescription(
         [
             lidar_node,
             joy_node,
             teleop_node,
+            cam_node
         ]
     )
