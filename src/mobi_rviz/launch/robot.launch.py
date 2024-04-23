6a58ea475ce10a5c5cbb681302608c74791ca32e
Flo2410 <florian@hye.dev>
2024-04-23 15:51:24 +0200

    Add micro-ROS agent to robot.launch

--- src/mobi_rviz/launch/robot.launch.py
+++ src/mobi_rviz/launch/robot.launch.py
@@ -23,16 +23,34 @@ def generate_launch_description():
     joy_node = Node(package="joy", executable="joy_node", parameters=[joy_params])
 
     # telepo (/joy -> /cmd_vel)
-    teleop_node = Node(package="teleop_twist_joy", executable="teleop_node", name="teleop_node", parameters=[joy_params])
+    teleop_node = Node(
+        package="teleop_twist_joy",
+        executable="teleop_node",
+        name="teleop_node",
+        parameters=[joy_params],
+    )
 
-    # cam_node = Node(package="mobipy", executable="cam", name="cam_publisher")
-    cam_node = Node(package="usb_cam", executable="usb_cam_node_exe", name="cam_node", parameters=[cam_params])
+    cam_node = Node(
+        package="usb_cam",
+        executable="usb_cam_node_exe",
+        name="cam_node",
+        parameters=[cam_params],
+    )
+
+    # micro ros agent
+    micro_ros_agent = Node(
+        package="micro_ros_agent",
+        executable="micro_ros_agent",
+        name="micro_ros_agent",
+        arguments=["serial", "--dev", "/dev/stm32usb"],
+    )
 
     return LaunchDescription(
         [
             lidar_node,
             joy_node,
             teleop_node,
-            cam_node
+            micro_ros_agent,
+            cam_node,
         ]
     )
